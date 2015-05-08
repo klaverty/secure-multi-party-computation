aa846b4f09622b5064acb00add9a04b953c1b888
gueltro <gueltro>
2015-05-08 11:13:44 -0400

    end

--- test.scm
+++ test.scm
@@ -1,23 +1,14 @@
+;;
+
 (define multiparty
   (make-multiparty 5))
 
 (for-each (lambda (player)
-	    (set-player-info! player 'input 1))
+	    (let ((r (random 2)))
+	      (begin
+	    (pp "Some player voted")
+	    (pp r)
+	    (set-player-info! player 'input r))))
 	  multiparty)
 
 (distributed-sum multiparty)
-
-#|
-(define player (make-player '()))
-
-(set-player-info! player 'input 1)
-(pp  (get-player-info player 'input))
-(define p (generate-rand-poly 27 10 19))
-(pp p)
-(define q (generate-rand-poly 27 10 19))
-(pp q)
-(define cp (point-value-poly p 19))
-(pp cp)
-(pp (reconstruct-poly cp 0 19))
-(pp (poly-add p q))
-|#
